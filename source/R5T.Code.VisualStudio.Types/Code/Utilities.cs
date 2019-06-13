28a13e7a0ef3773099f6fae9b7ff23c766464bfa
MinexAutomation <minex.automation@gmail.com>
2019-06-13 12:55:18 -0400

    Added ProjectName and SolutionName functionality.

--- source/R5T.Code.VisualStudio.Types/Code/Utilities.cs
+++ source/R5T.Code.VisualStudio.Types/Code/Utilities.cs
@@ -2,13 +2,44 @@
 using System.Xml;
 
 using R5T.NetStandard;
+using R5T.Code.VisualStudio.Extensions;
 using R5T.Code.VisualStudio.IO;
+
+using PathUtilities = R5T.NetStandard.IO.Paths.Utilities;
 
 
 namespace R5T.Code.VisualStudio
 {
     public static class Utilities
     {
+        public static ProjectName GetProjectName(ProjectFileName projectFileName)
+        {
+            var projectName = PathUtilities.GetFileNameWithoutExtension(projectFileName).Value.AsProjectName();
+            return projectName;
+        }
+
+        public static ProjectName GetProjectName(ProjectFilePath projectFilePath)
+        {
+            var projectFileName = PathUtilities.GetFileName(projectFilePath).AsProjectFileName();
+
+            var projectName = Utilities.GetProjectName(projectFileName);
+            return projectName;
+        }
+
+        public static SolutionName GetSolutionName(SolutionFileName solutionFileName)
+        {
+            var solutionName = PathUtilities.GetFileNameWithoutExtension(solutionFileName).Value.AsSolutionName();
+            return solutionName;
+        }
+
+        public static SolutionName GetSolutionName(SolutionFilePath solutionFilePath)
+        {
+            var solutionFileName = PathUtilities.GetFileName(solutionFilePath).AsSolutionFileName();
+
+            var projectName = Utilities.GetSolutionName(solutionFileName);
+            return projectName;
+        }
+
         /// <summary>
         /// Determines if a project-file has a version property.
         /// Returns true and the version if so, false and <see cref="VersionHelper.None"/> if not.
