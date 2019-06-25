8bbd04af751db88e850d01ce09bab8ef48fc23b3
MinexAutomation <minex.automation@gmail.com>
2019-06-25 12:36:36 -0400

    Removed extraneous R5T.NetStandard.Extensions project reference (follow-through on deletion of R5T.NetStandard.Extensions repository/library) Added new dependency project references. Moved dependendcy projects to a dependencies solution folder.

---
+++ source/R5T.Code.VisualStudio.Types/Code/Classes/ProjectFileModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+
+
+namespace R5T.Code.VisualStudio
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// Built around an in-memory <see cref="XmlDocument"/> and allows using XML-related functions on the XML document project file content.
+    /// </remarks>
+    public class ProjectFileModel
+    {
+        #region Static
+
+
+
+        #endregion
+
+
+        public XmlDocument XmlDocument { get; }
+
+
+        private ProjectFileModel(XmlDocument xmlDocument)
+        {
+            this.XmlDocument = xmlDocument;
+        }
+    }
+}
